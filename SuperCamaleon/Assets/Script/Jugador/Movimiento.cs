4b05de8d8d23186c5b4a918fc43a40c09496ef60
Carlos Borunda <diastipicosgameplays@gmail.com>
2023-07-08 17:08:26 -0600

    [update] avemus movimiento

--- SuperCamaleon/Assets/Script/Jugador/Movimiento.cs
+++ SuperCamaleon/Assets/Script/Jugador/Movimiento.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    
+    public float speed;
+    private Rigidbody rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw ("Vertical");
         
+
+        if(hor != 0.0f || ver != 0.0f)
+        {
+            Vector3 dir = transform.forward * ver + transform.right * hor;
+
+            rb.MovePosition(transform.position + dir * speed * Time.deltaTime);
+        }
+
     }
 }
