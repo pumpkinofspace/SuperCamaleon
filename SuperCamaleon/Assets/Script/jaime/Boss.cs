20614b2a4de1ebdbe24f27ffc3ec5c6955939f25
Carlos Borunda <diastipicosgameplays@gmail.com>
2023-07-10 20:18:38 -0600

    [update] se agregaron maquina estados del jefe
    maquina de estados del jefe han sido agregados, faltan conexciones.

--- SuperCamaleon/Assets/Script/jaime/Boss.cs
+++ SuperCamaleon/Assets/Script/jaime/Boss.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Boss : MonoBehaviour
 {
     public int rutina;
-    public float conometro;
+    public float cronometro;
     public float time_rutinas;
     public Animator ani;
     public Quaternion angulo;
@@ -29,7 +29,7 @@ public class Boss : MonoBehaviour
     public float jump_distance;
     public bool direction_skill;
     
-    //gaydar//
+    //gaydar////fireball//
     public GameObject fire_ball;
     public GameObject Point;
     public List<GameObject> pool2 = new List<GameObject>();
@@ -48,5 +48,115 @@ public class Boss : MonoBehaviour
         ani = GetComponent<Animator>();
         target = GameObject.Find("Link");
     }
+
+    public void Comportamiento_Boss()
+    {
+        if(Vector3.Distance(transform.position, target.transform.position) < 15)
+        {
+            var lookPos = target.transform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            Point.transform.LookAt(target.transform.position);
+            musica.enabled = true;
+
+            //activar el boss
+            if(Vector3.Distance(transform.position, target.transform.position) > 1 && !atacando)
+            {
+                switch (rutina)
+                {
+                    //caminar hacia el jugador
+                    case 0:
+                        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+                        ani.SetBool("walk", true);
+                        ani.SetBool("run", false);
+
+                        if(transform.rotation == rotation)
+                        {
+                            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                        }
+
+                        ani.SetBool("attack", false);
+                        cronometro += 1 * Time.deltaTime;
+                        if(cronometro > time_rutinas)
+                        {
+                            rutina = Random.Range(0, 5);
+                            cronometro = 0;
+                        }
+                        break;
+
+                    // ir en putiza osea correr//
+                    case 1:
+
+                        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+                        ani.SetBool("walk", false);
+                        ani.SetBool("run", true);
+
+                        if (transform.rotation == rotation)
+                        {
+                            transform.Translate(Vector3.forward * speed * 2 * Time.deltaTime);
+                        }
+
+                        ani.SetBool("attack", false);
+                        cronometro += 1 * Time.deltaTime;
+                        if (cronometro > time_rutinas)
+                        {
+                            rutina = Random.Range(0, 5);
+                            cronometro = 0;
+                        }
+                        break;
+
+                    // rayo jotizador osea lanzallamas
+                    case 2:
+                        ani.SetBool("walk", false);
+                        ani.SetBool("run", false);
+                        ani.SetBool("attack", true);
+                        ani.SetFloat("skills", 0);
+
+                        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+                        rango.GetComponent<CapsuleCollider>().enabled = false;
+
+
+                        break;
+
+                    //caso ataque salto
+                    case 3:
+                       
+                        if( fase == 2)
+                        {
+                            jump_distance += 1 * Time.deltaTime;
+                            ani.SetBool("walk", false);
+                            ani.SetBool("run", false);
+                            ani.SetBool("attack", true);
+                            ani.SetFloat("skill", 0);
+                            hit_Select = 3;
+                            rango.GetComponent<CapsuleCollider>().enabled = false;
+
+                            if (direction_skill)
+                            {
+                                if(jump_distance < 1f)
+                                {
+                                    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+                                }
+
+                                transform.Translate(Vector3.forward * 8 * Time.deltaTime);
+                            }
+
+
+
+                        }
+                        else
+                        {
+                            rutina = 0;
+                            cronometro = 0;
+                        }
+
+                        break;
+                }
+            }
+
+
+        }
+    }
+
 
 }
