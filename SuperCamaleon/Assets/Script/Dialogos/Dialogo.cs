971cdebfb796609d28bf2f6a8bb37c5b8a9519f0
Carlos Borunda <diastipicosgameplays@gmail.com>
2023-07-21 21:17:52 -0600

    [upgrade] sistema de dialogo basico
    faltan detalles

--- SuperCamaleon/Assets/Script/Dialogos/Dialogo.cs
+++ SuperCamaleon/Assets/Script/Dialogos/Dialogo.cs
@@ -1,18 +1,89 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Dialogo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject marcaDeDialogo;
+    [SerializeField] private GameObject panelDialogo;
+    [SerializeField] private TMP_Text textoDialogo; 
+    [SerializeField, TextArea(4, 6)] private string[] lineasDialogo;
 
+    private bool playerEstaCercas;
+    private bool dialogoEstado;
+    private int lineaIndex;
+
+    public float tiempoEscritura = 0.05f;
     // Update is called once per frame
     void Update()
     {
+        if(playerEstaCercas && Input.GetButtonDown("Fire1"))
+        {
+            if (!dialogoEstado)
+            {
+                StartDialogo();
+            }else if (textoDialogo.text == lineasDialogo[lineaIndex])
+            {
+                NextDialogo();
+            }
+           
+        }
+    }
+
+    private void StartDialogo()
+    {
+        dialogoEstado = true;
+        marcaDeDialogo.SetActive(false);
+        panelDialogo.SetActive(true);
+        lineaIndex = 0;
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogo()
+    {
+        lineaIndex++;
+        if(lineaIndex < lineasDialogo.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            dialogoEstado = false;
+            panelDialogo.SetActive(false);
+            marcaDeDialogo.SetActive(true);
+
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        textoDialogo.text = string.Empty;
+
+        foreach(char ch in lineasDialogo[lineaIndex])
+        {
+            textoDialogo.text += ch;
+            yield return new WaitForSeconds(tiempoEscritura);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            marcaDeDialogo.SetActive(true);
+            playerEstaCercas = true;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            marcaDeDialogo.SetActive(false);
+            playerEstaCercas = false;
+        }
         
     }
 }
