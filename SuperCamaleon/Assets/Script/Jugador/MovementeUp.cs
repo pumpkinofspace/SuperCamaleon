5e1146d23920af6805a1cf456794c7af62445498
Carlos Borunda <diastipicosgameplays@gmail.com>
2024-01-05 22:41:20 -0600

    [update]
    se incorporo la gravedad al jugar de tipo character controller.

--- SuperCamaleon/Assets/Script/Jugador/MovementeUp.cs
+++ SuperCamaleon/Assets/Script/Jugador/MovementeUp.cs
@@ -7,49 +7,46 @@ public class MovementeUp : MonoBehaviour
     public CharacterController controller;
 
     public float speed;
-
     public float turnSmoothTime = 0.1f;
-
     float turnSmoothSuavecity;
 
-    public float gravedad = 9.81f; 
-
-
+    public float gravity = 9.81f;
+    float yVelocity = 0f;
+    bool isGrounded;
 
     public Transform cam;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direcion = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        transform.position.y += gravedad * Time.deltaTime;
-
-
-        if(direcion.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
-
-            float targetAngle = Mathf.Atan2(direcion.x, direcion.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothSuavecity, turnSmoothTime);
-
-
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            
-
 
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        // Aplicar gravedad
+        isGrounded = controller.isGrounded;
+
+        if (!isGrounded)
+        {
+            yVelocity -= gravity * Time.deltaTime;
+        }
+        else
+        {
+            yVelocity = -0.5f; // Si está en el suelo, restablecer la velocidad a un valor pequeño
+        }
+
+        Vector3 verticalVelocity = Vector3.up * yVelocity;
+        controller.Move(verticalVelocity * Time.deltaTime);
     }
 }
